379ae9a985ce89491e11a0f08da754ebe51076f9
Jonathan Vargas <jonathanv3232@gmail.com>
2018-01-08 18:12:36 -0500

    adds shift press functionality

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+let lastChecked;
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    //loop over checkboxes
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        console.log(this);
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
